2c9e39eade70f8477a7bd5e83e4990f8880fb8e5
Douglas Lee <lee010073@gmail.com>
2020-09-08 12:20:12 +0800

    added about-me recommendation

---
+++ app.js
@@ -0,0 +1,10 @@
+$(".site-main .about-area .owl-carousel").owlCarousel({
+  loop: true,
+  autoplay: true,
+  dots: true,
+  responsive: {
+    0: { items: 1 },
+    544: { items: 2 },
+    1000: { items: 3 },
+  },
+});

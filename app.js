6c54600a671f8cb22a1658cb2ede77e301ead6d0
Douglas Lee <lee010073@gmail.com>
2020-09-08 15:32:45 +0800

    finished the gallery section

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*Owl Carousel javascript */
 $(".site-main .about-area .owl-carousel").owlCarousel({
   loop: true,
   autoplay: true,
@@ -7,4 +8,20 @@ $(".site-main .about-area .owl-carousel").owlCarousel({
     544: { items: 2 },
     1000: { items: 3 },
   },
+});
+
+/*isotope javscript */
+$(document).ready(function() {
+  let $btns = $(".project-area .button-group button");
+  $btns.click(function(e) {
+    $(".project-area .button-group button").removeClass("active");
+    e.target.classList.add("active");
+
+    let selector = $(e.target).attr("data-filter");
+    $(".project-area .grid").isotope({
+      filter: selector,
+    });
+
+    return false;
+  });
 });
